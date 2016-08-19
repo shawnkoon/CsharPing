50bf97a0496ce631a242193067d2e5af0df86f0e
shawnkoon <shawn9405@gmail.com>
2016-08-19 14:25:27 -0700

    Ping works correctly

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Net.NetworkInformation;
 
 namespace ConsoleApplication
 {
@@ -6,7 +8,44 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 1; i <= 20; i++)
+            {
+                Ping ping = new Ping();
+
+                string webToPing = "tistory.co.kr";
+
+                string msg = "Pinging attempt " + i + " ["+webToPing+"]";
+
+                try
+                {
+                    long rtt = ping.SendPingAsync(webToPing).Result.RoundtripTime;
+                    if(rtt > 80)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(msg + " RTT = " + rtt.ToString() + "ms");
+                        Console.ResetColor();
+                    }
+                    else if(rtt > 50 && rtt <= 80)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine(msg + " RTT = " + rtt.ToString() + "ms");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(msg + " RTT = " + rtt.ToString() + "ms");
+                        Console.ResetColor();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine(msg + "* * * * 0ms");
+                }
+
+                Thread.Sleep(1000);
+            }
+
         }
     }
 }
