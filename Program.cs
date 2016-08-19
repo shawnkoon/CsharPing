5d6f5455bc3131a4f7bfa2a4021455349e53a225
shawnkoon <shawn9405@gmail.com>
2016-08-19 15:20:39 -0700

    Keeps track of Max/Min/Cur/Avg Ping amount

--- Program.cs
+++ Program.cs
@@ -8,7 +8,12 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            for(int i = 1; i <= 20; i++)
+            int maxPing = -1;
+            int minPing = -1;
+            int totalPing = 0;
+            int numTimesPinged = 0;
+
+            for(int i = 1; i <= 40; i++)
             {
                 Ping ping = new Ping();
 
@@ -16,9 +21,23 @@ namespace ConsoleApplication
 
                 string msg = "Pinging attempt " + i + " ["+webToPing+"]";
 
+                numTimesPinged = i;
+
                 try
                 {
-                    long rtt = ping.SendPingAsync(webToPing).Result.RoundtripTime;
+                    int rtt = (Int32)ping.SendPingAsync(webToPing).Result.RoundtripTime;
+
+                    if(maxPing == -1 || minPing == -1)
+                    {
+                        maxPing = rtt;
+                        minPing = rtt;
+                    }
+
+                    totalPing +=  rtt;
+
+                    maxPing = checkforMax(rtt,maxPing);
+                    minPing = checkforMin(rtt,minPing);
+
                     if(rtt > 80)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
@@ -37,15 +56,59 @@ namespace ConsoleApplication
                         Console.WriteLine(msg + " RTT = " + rtt.ToString() + "ms");
                         Console.ResetColor();
                     }
+
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Max Ping: "+maxPing+"\tMin Ping: "+minPing+"\tCur Ping: "+rtt+"\tAvg Ping: "+(totalPing/numTimesPinged));
+                    Console.ResetColor();
+
+
                 }
                 catch
                 {
                     Console.WriteLine(msg + "* * * * 0ms");
                 }
 
+                
+
+
                 Thread.Sleep(1000);
             }
 
-        }
+        }// End of Main.
+
+        public static int checkforMax(int rtt, int oldMax)
+        {
+            int newMax;
+
+            if(rtt > oldMax)
+            {
+                newMax = rtt;
+            }
+            else
+            {
+                newMax = oldMax;
+            }
+
+            return newMax;
+        }// End of checkforMax
+
+        public static int checkforMin(int rtt, int oldMin)
+        {
+            int newMin;
+
+            if(rtt < oldMin)
+            {
+                newMin = rtt;
+            }
+            else
+            {
+                newMin = oldMin;
+            }
+
+            return newMin;
+        }// End of checkforMin
+
+
     }
 }
